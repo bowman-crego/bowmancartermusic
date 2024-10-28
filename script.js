2b18af6763c190b51888c2e76a09596cb7fac5ee
bowman-crego <bowmanccrego@gmail.com>
2024-10-28 12:39:44 -0700

    changes made to CSS and index.html files

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = 'Bowman';
+
+const names = ['Bowman', 'Jen', 'Thad', 'Caleb'];
+console.log(names);
+console.log(names[1]);
+console.log(names[0]);
+console.log(names[3]);
+
+names [3] = 'Carter';
+
+console.log(`The fourth name is ${names[3]}.`);
